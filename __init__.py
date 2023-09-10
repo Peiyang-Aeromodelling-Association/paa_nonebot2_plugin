058efbbb234253a5ca8c54e5cdf74725bc1d4f5b
lcf <kevinfinley@163.com>
2023-09-10 19:48:59 +0800

    添加了入群私信

--- __init__.py
+++ __init__.py
@@ -1,11 +1,11 @@
 import os
-from nonebot import on_message, on_command
+from nonebot import on_message, on_command, on_notice
 from nonebot.log import logger
 from nonebot.adapters.onebot.v11 import Message
 from nonebot.params import CommandArg
 from nonebot.plugin import on_command
 
-from nonebot.adapters.onebot.v11 import Bot, MessageEvent
+from nonebot.adapters.onebot.v11 import Bot, MessageEvent, GroupIncreaseNoticeEvent
 
 from nonebot.plugin import PluginMetadata
 
@@ -23,6 +23,26 @@ get_repo = on_command("仓库", priority=15, block=True)
 get_wechat = on_command("公众号", priority=15, block=True)
 get_signup = on_command("报名", priority=15, block=True)
 get_advertise = on_command("宣传片", priority=15, block=True)
+
+# if a person joins the group 537857732, send a welcome message
+welcome = on_notice(priority=15, block=True)
+
+
+@welcome.handle()
+async def handle_welcome(bot: Bot, event: GroupIncreaseNoticeEvent):
+    if event.group_id == 537857732:  # 537857732 is the group id of PAA
+        signup_qr_code_path = os.path.join(resource_dir, "signup.png")
+        wechat_qr_code_path = os.path.join(resource_dir, "wechat.png")
+        # send private message
+        await bot.send_private_msg(user_id=event.user_id,
+                                   group_id=event.group_id,
+                                   message=Message(f"欢迎加入北航航模协会！我是外联bot。\n"
+                                                   f"报名二维码：[CQ:image,file=file:///{signup_qr_code_path}]\n"
+                                                   f"公众号二维码：[CQ:image,file=file:///{wechat_qr_code_path}]\n"
+                                                   f"文档：https://paa.abc235.site/documentation/\n"
+                                                   f"github：https://github.com/Peiyang-Aeromodelling-Association/\n"
+                                                   f"欢迎进一步交流！\n"))
+
 
 @get_doc.handle()
 async def handle_doc(bot: Bot, event: MessageEvent):
@@ -44,6 +64,7 @@ async def handle_wechat(bot: Bot, event: MessageEvent):
 async def handle_signup(bot: Bot, event: MessageEvent):
     qr_code_path = os.path.join(resource_dir, "signup.png")
     await get_signup.send(message=Message(f"[CQ:image,file=file:///{qr_code_path}]"))
+
 
 @get_advertise.handle()
 async def handle_advertise(bot: Bot, event: MessageEvent):
